e9dad120867bdb3efced26e420248c8da3d20487
jrizza88 <jetorres88@gmail.com>
2016-06-21 08:10:30 -0400

    files updated

---
+++ assets/javascript/giphy.js
@@ -0,0 +1,145 @@
+$(document).ready(function(){ 
+
+var singerArray = ['Prince', 'Whitney Houston', 'Michael Jackson', 'Nas', 'James Brown', 'Beyonce']; 
+
+    function appendNewButton(newGif){ 
+        var createButton = $('<button>')
+        createButton.attr('type', "button");
+        createButton.addClass('btn btn-default');
+        createButton.addClass('gif');
+        createButton.attr('data-name', newGif);
+        createButton.text(newGif);
+        $('#buttonsView').append(createButton);
+    };
+
+    function renderButtons(){ 
+      for (var i = 0; i < singerArray.length; i++){
+        appendNewButton(singerArray[i])
+        }
+    };
+
+function displaySinger(){
+        $('#gifDisplay').empty();
+        var singer = $(this).attr('data-name'); // here I create a variable to grab the "name" info from the html buttons tags
+        var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + singer + "&api_key=dc6zaTOxFJmzC&limit=10"; // searches singer / array of 10
+        
+    $.ajax({
+            url: queryURL,
+            method: 'GET'
+    }).done(function(response) {
+        console.log(response);
+
+        var results = response.data;
+        for (var i = 0; i <results.length; i++) {
+            var singerDiv = $('<div>');
+            var p = $('<p>').text("Rating: " + results[i].rating);
+            var singerImage = $('<img>');
+            var singerStill = results[i].images.fixed_height_still.url;
+            var singerActive = results[i].images.fixed_height.url;
+           // var singerState = results[i].images.fixed_height.url;
+            
+            
+            singerImage.attr('data-still', singerStill);
+            singerImage.attr('data-animate', singerActive);
+            singerImage.attr('data-state', 'still');
+            singerImage.attr('class', 'singerImage');
+            console.log('line 46');
+            singerImage.attr('src', singerActive);
+           // singerImage.attr('data-state', 'animate');
+            //singerImage.attr('src', singerStill);
+            //singerImage.attr('src', singerActive);
+            
+            
+            singerDiv.addClass('gifToggle'); 
+
+            singerDiv.append(p);
+            singerDiv.append(singerImage);
+
+            $('#gifDisplay').prepend(singerDiv);
+
+        }   
+    });   
+ }
+
+    function gifState () {
+    var state = $(this).attr('data-state'); 
+    console.log(state);
+    if (state === 'still'){
+            $(this).attr('src', $(this).data('animate'));
+            $(this).attr('data-state', 'animate');
+           console.log(state);
+        }else{
+            $(this).attr('src', $(this).data('still'));
+            $(this).attr('data-state', 'still');
+            console.log(state);
+        }
+
+};
+ 
+    renderButtons();
+
+  $('#searchButton').on('click', function(){
+    $('#gif-input').text('');
+    $('#gifView').empty();
+
+    var gifSearch = $('#gif-input').val().trim();
+    singerArray.push(gifSearch);
+    appendNewButton(gifSearch);
+    
+
+    var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + gifSearch + "&api_key=dc6zaTOxFJmzC&limit=10";
+    $.ajax({
+              url: queryURL,
+              method: 'GET'
+          }).done(function(response) {
+            console.log(response);
+
+            var results = response.data;
+
+             $('#gifDisplay').empty();
+
+            for (var i = 0; i <results.length; i++) {
+            var singerDiv = $('<div>');
+            var p = $('<p>').text("Rating: " + results[i].rating);
+            var singerImage = $('<img>');
+            var singerStill = results[i].images.fixed_height_still.url;
+            var singerActive = results[i].images.fixed_height.url;
+           
+            singerDiv.addClass('gifToggle');
+            singerImage.attr('data-still', singerStill);
+            singerImage.attr('data-animate', singerActive);
+            singerImage.attr('data-state', 'still');
+            singerImage.attr('src', singerActive);
+           // singerImage.attr('src', singerStill);
+           // singerImage.attr('src', singerActive);
+        
+
+            singerDiv.append(p);
+            singerDiv.append(singerImage);
+
+            $('#gifDisplay').prepend(singerDiv);
+
+              }
+
+              if (results.length == 0) {
+                $('#gifDisplay').text("Sorry, no gifs found")
+             gifState ();
+              };
+return false;
+          });
+        
+    
+  });
+
+
+
+  $(document).on('click', '.gifToggle', gifState);
+  $(document).on('click', '.gif', displaySinger);
+  
+
+ });
+
+
+
+
+
